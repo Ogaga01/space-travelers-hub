d01405fa03b1e8f3c3dbfa62be956069595ed607
Ogaga <iyaraogaga69@gmail.com>
2023-01-02 17:11:17 +0100

    My profile missions

---
+++ src/components/MissionReservedList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const MissionReservedList = () => {
+    const missions = useSelector((state)=>{return state.missionSlice.missions})
+
+    const joinedMissions = missions.filter((mission)=>{return mission.joined = true})
+
+    return (
+      <div className="my-profile-item">
+        <h2>My Missions</h2>
+        {joinedMissions.length < 1 ? (
+          <p>Be the first to join a mission</p>
+        ) : (
+          <table className="my-profile-mission-table">
+            <tbody>
+              {joinedMissions.map((mission, index) => {
+                const count = index;
+                return (
+                  <tr key={count}>
+                    <td className="my-profil-mission">{mission.missionName}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    );
+};
+
+export default MissionReservedList;
---
+++ src/components/RocketReservedList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RocketReservedList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RocketReservedList;
