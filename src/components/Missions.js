0ac1450a50afc8f0f1fe4e136dab6bd9d27aa48f
Ogaga <iyaraogaga69@gmail.com>
2023-01-02 14:07:50 +0100

    Create mission data

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Missions = () => {
+        const missions = useSelector((state) => {
+          return state.missionSlice.missions;
+        });
+
+
     return (
         <div>
             
